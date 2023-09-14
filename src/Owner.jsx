ffaba6927518147fe4558672991ab9ad033b556c
khajornphop9 <khajornphop.nut@gmail.com>
2023-09-14 13:17:15 +0700

    update Owner.jsx, add picture, add navbar

--- src/Owner.jsx
+++ src/Owner.jsx
@@ -1,22 +1,54 @@
-import React from 'react'
-import Layout from './components/Layout'
-
+import React from "react";
+import Layout from "./components/Layout";
 
 const Owner = () => {
   return (
-    
     <div>
       <Layout>
-        <div className='biography'>
-        <h1>14_Kamontodsapon (Nut/นัท)</h1>
-        <img className='bio-img' src={"https://legacy.reactjs.org/logo-og.png"} alt="" />
-        <h5>Short Biography</h5>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, aperiam quod nam repudiandae quo quisquam libero perferendis fugit, deserunt eaque in similique impedit perspiciatis? Recusandae pariatur adipisci explicabo atque. Est!</p>
-      </div>
+        <div className="biography">
+          <h1>JSD#5-14_Kamontodsapon (Nut/นัท)</h1>
+          <img
+            className="bio-img"
+            src={"./src/assets/1662377790417.jpg"}
+            alt=""
+          />{" "}
+          {/*"https://legacy.reactjs.org/logo-og.png"*/}
+          <h5>Short Biography</h5>
+        </div>
+        <div>
+          <p>Name: NUT</p>
+
+          <p>Profession: Aspiring Junior Software Developer</p>
+
+          <p>Education: Diploma of Computer Technique</p>
+
+          <p>
+            Interests: Listening to music, watching movies, and exploring new
+            places through travel.
+          </p>
+
+          <p>Notable Accomplishments:</p>
+          <ul>
+            <li>
+              Proficient in Microsoft Excel in the past, though skills may need
+              a refresh.
+            </li>
+            <li>
+              Previously studied Chinese language, albeit needing a refresher.
+            </li>
+            <li>
+              Currently in the process of learning the Russian language, with
+              basic knowledge.
+            </li>
+            <li>
+              Pursuing a career in software development through Generation
+              Thailand's Junior Software Developer Bootcamp.
+            </li>
+          </ul>
+        </div>
       </Layout>
     </div>
-  
-  )
-}
+  );
+};
 
-export default Owner
+export default Owner;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import '../assets/style/navbar.css'
+
 
 const Navbar = () => {
   return (
-   <ul>
-    <li><a href={'/owner'}>Owner</a></li>
-    <li><a href={'/'}>Home</a></li>
+   <ul className='navbar-ul'>
+    <li className='navbar-li'><a className='navbar-a' href={'/owner'}>Owner</a></li>
+    <li className='navbar-li'><a className='navbar-a' href={'/'}>Home</a></li>
    </ul>
   )
 }
